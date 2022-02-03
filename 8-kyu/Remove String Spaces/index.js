7475775038dc5778c0df30c2d55a8264a1b82392
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-02-03 10:49:05 +0200

    Remove String Spaces
    8-kyu solution

---
+++ 8-kyu/Remove String Spaces/index.js
@@ -0,0 +1,11 @@
+/*
+Simple, remove the spaces from the string, then return the resultant string.
+
+https://www.codewars.com/kata/57eae20f5500ad98e50002c5/javascript
+*/
+
+// My solution
+
+function noSpace(x){
+  return x.split(' ').join('')
+}
