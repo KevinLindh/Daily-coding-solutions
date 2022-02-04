e337ff3356514c881678708165d9dfc00eeb4731
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-02-04 18:44:41 +0200

    Convert a string to an array kata solution
    kyu-8 solution

---
+++ 8-kyu/Convert a string to an array/index.js
@@ -0,0 +1,15 @@
+/*
+Write a function to split a string and convert it into an array of words. For example:
+
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+*/
+
+// My solution
+
+function stringToArray(string){
+  return string.split(' ');
+}
