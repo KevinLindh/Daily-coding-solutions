fb1ec974e285a898c08780eddee955ddf66e7fdb
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-02-04 10:32:59 +0200

    Unique In Order solution
    6-kyu kata solution

---
+++ 6-kyu/Unique In Order/index.js
@@ -0,0 +1,27 @@
+/*
+Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+https://www.codewars.com/kata/54e6533c92449cc251001667/javascript
+
+*/
+
+// My solution
+
+var uniqueInOrder=function(iterable){
+let newArr = [];
+  for (let i=0; i<iterable.length; i++){
+    if (newArr.length === 0){
+      newArr.push(iterable[i])
+    } else if (newArr.lastIndexOf(iterable[i]) === (newArr.length - 1)){
+    } else {
+      newArr.push(iterable[i])
+    }
+  }
+  return newArr;
+}
