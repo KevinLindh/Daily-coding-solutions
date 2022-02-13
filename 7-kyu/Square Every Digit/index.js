a15d2e6ec3b5c38385ca4ccf1065c47280d4ae5b
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-02-13 09:23:19 +0200

    Square Every Digit solution
    7-kyu solution

---
+++ 7-kyu/Square Every Digit/index.js
@@ -0,0 +1,23 @@
+/*
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+Note: The function accepts an integer and returns an integer
+
+https://www.codewars.com/kata/546e2562b03326a88e000020/javascript
+*/
+
+// My solution
+
+function squareDigits(num){
+  let string = num.toString();
+  let newArr = [];
+  //split every variable of the number into individual variables (loop)
+  for(let i=0; i < string.length; i++){
+    //take each variable and get the squareroot of the number
+    newArr[i] = string[i] * string[i];
+  }
+  //join together square root of numbers
+  return Number(newArr.join(""));
+}
