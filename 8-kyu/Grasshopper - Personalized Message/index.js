c3c3a430285665d38875931f40467136b417bdbd
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-02-22 09:08:13 +0200

    Grasshopper - Personalized Message solution
    8-kyu solution

---
+++ 8-kyu/Grasshopper - Personalized Message/index.js
@@ -0,0 +1,22 @@
+/*
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest'
+
+https://www.codewars.com/kata/5772da22b89313a4d50012f7/javascript
+*/
+
+//My solution
+
+function greet (name, owner) {
+    if (name==owner){
+    return "Hello boss"
+    }
+    else{
+    return "Hello guest"
+    }
+}
