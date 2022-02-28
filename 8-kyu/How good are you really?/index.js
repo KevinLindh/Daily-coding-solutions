446406019d95b5a157da3626674a9d0a2acf4d6a
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-02-28 08:43:07 +0200

    How good are you really? solution
    8-kyu solution

---
+++ 8-kyu/How good are you really?/index.js
@@ -0,0 +1,26 @@
+/*
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+Note:
+
+Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+https://www.codewars.com/kata/5601409514fc93442500010b/javascript
+*/
+
+// My solution
+function betterThanAverage(classPoints, yourPoints) {
+  let count = 0;
+  for(let i=0; i<classPoints.length; i++){
+    count += classPoints[i];
+  }
+  if(count/classPoints.length > yourPoints){
+    return false;
+  } else {
+    return true;
+  }
+}
