15d6fec991cb902c67f6d5e97df463efd296926d
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-03-03 09:03:44 +0200

    Sum of a sequence solution
    7-kyu solution

---
+++ 7-kyu/Sum of a sequence/index.js
@@ -0,0 +1,36 @@
+/*
+Your task is to make function, which returns the sum of a sequence of integers.
+
+The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
+
+If begin value is greater than the end, function should returns 0
+
+Examples
+
+2,2,2 --> 2
+2,6,2 --> 12 (2 + 4 + 6)
+1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+1,5,3  --> 5 (1 + 4)
+
+This is the first kata in the series:
+
+    Sum of a sequence (this kata)
+    Sum of a Sequence [Hard-Core Version]
+
+https://www.codewars.com/kata/586f6741c66d18c22800010a/javascript
+*/
+
+// My solution
+const sequenceSum = (begin, end, step) => {
+  let progress = begin;
+  let answer = begin;
+  if (begin>end){
+    return 0;
+  } else {
+  while (progress+step <= end){
+    progress = progress + step;
+    answer +=progress;
+    }
+  }
+  return answer;
+};
