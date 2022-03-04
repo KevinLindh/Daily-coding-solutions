e6c3a733c09bc7d4c912175b3040dc1c5ccd092b
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-03-04 11:03:50 +0200

    Sum of all the multiples of 3 or 5 solution
    7-kyu solution

---
+++ 7-kyu/Sum of all the multiples of 3 or 5/index.js
@@ -0,0 +1,26 @@
+/*
+Your task is to write function findSum.
+
+Upto and including n, this function will return the sum of all multiples of 3 and 5.
+
+For example:
+
+findSum(5) should return 8 (3 + 5)
+
+findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+
+https://www.codewars.com/kata/57f36495c0bb25ecf50000e7/javascript
+*/
+
+// My solution
+function findSum(n) {
+  let sum = 0;
+  
+  for(let i = 0; i <= n; i++){
+    if(i % 3 === 0 || i % 5 === 0){
+      sum += i;
+    }
+  }
+  
+  return sum;
+}
