f03bbd13291dc0ba8ed71e0424fb0178d4e89230
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-03-05 09:31:41 +0200

    Thinkful - Logic Drills: Traffic light solution
    8-kyu solution

---
+++ 8-kyu/Thinkful - Logic Drills: Traffic light/index.js
@@ -0,0 +1,26 @@
+/*
+You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+For example, update_light('green') should return 'yellow'.
+
+Note: Use "" instead of '' in C++.
+
+https://www.codewars.com/kata/58649884a1659ed6cb000072/javascript
+*/
+
+//My solution
+function updateLight(current) {
+  switch(current) {
+    case "green":
+      return "yellow";
+      break;
+    case "yellow":
+      return "red";
+      break;
+    case "red":
+      return "green";
+      break;
+  }
+}
