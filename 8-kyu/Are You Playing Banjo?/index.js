3ca6ab115a63207c469e77310448eb755d391b9f
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-03-06 09:27:04 +0200

    Are You Playing Banjo? solution
    8-kyu solution

---
+++ 8-kyu/Are You Playing Banjo?/index.js
@@ -0,0 +1,23 @@
+/*
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+
+Names given are always valid strings.
+
+https://www.codewars.com/kata/53af2b8861023f1d88000832/javascript
+*/
+
+// My solution
+function areYouPlayingBanjo(name) {
+  let regex1 = /^r/i;
+ if(regex1.test(name)){
+   return `${name} plays banjo`
+ } else {
+   return `${name} does not play banjo`
+ }
+}
