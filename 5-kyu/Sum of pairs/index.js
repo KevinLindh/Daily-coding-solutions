9a94fbb2a36bcee87ef5d046f1113c9826e9eeff
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-03-09 10:30:16 +0200

    Sum of pairs solution
    5-kyu solution

---
+++ 5-kyu/Sum of pairs/index.js
@@ -0,0 +1,50 @@
+/*
+Sum of Pairs
+
+Given a list of integers and a single sum value, return the first two values (parse from the left please) in order of appearance that add up to form the sum.
+
+sum_pairs([11, 3, 7, 5],         10)
+#              ^--^      3 + 7 = 10
+== [3, 7]
+
+sum_pairs([4, 3, 2, 3, 4],         6)
+#          ^-----^         4 + 2 = 6, indices: 0, 2 *
+#             ^-----^      3 + 3 = 6, indices: 1, 3
+#                ^-----^   2 + 4 = 6, indices: 2, 4
+#  * entire pair is earlier, and therefore is the correct answer
+== [4, 2]
+
+sum_pairs([0, 0, -2, 3], 2)
+#  there are no pairs of values that can be added to produce 2.
+== None/nil/undefined (Based on the language)
+
+sum_pairs([10, 5, 2, 3, 7, 5],         10)
+#              ^-----------^   5 + 5 = 10, indices: 1, 5
+#                    ^--^      3 + 7 = 10, indices: 3, 4 *
+#  * entire pair is earlier, and therefore is the correct answer
+== [3, 7]
+
+Negative numbers and duplicate numbers can and will appear.
+
+NOTE: There will also be lists tested of lengths upwards of 10,000,000 elements. Be sure your code doesn't time out.
+
+https://www.codewars.com/kata/54d81488b981293527000c8f/javascript
+*/
+
+//My solution
+function sumPairs(ints, s) {
+  if(ints.length < 2){ 
+    return undefined;
+    } else {
+      let intSet = new Set();
+      intSet.add(ints[0]);
+      for (let i = 1; i < ints.length; i++){
+        let needed = s-ints[i];
+        if(intSet.has(needed)){
+          return [needed, ints[i]];
+        }
+        intSet.add(ints[i]);
+      }
+    }
+  return undefined;
+}
