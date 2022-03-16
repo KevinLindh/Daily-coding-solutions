2d3b1e22301decb4992e472a82fae0e849547aec
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-03-16 10:51:46 +0200

    Multiplying numbers as strings solution
    4-kyu solution

---
+++ 4-kyu/Multiplying numbers as strings/index.js
@@ -0,0 +1,15 @@
+/*
+This is the first part. You can solve the second part here when you are done with this. Multiply two numbers! Simple!
+
+    The arguments are passed as strings.
+    The numbers may be way very large
+    Answer should be returned as a string
+    The returned "number" should not start with zeros e.g. 0123 is invalid
+
+Note: 100 randomly generated tests!
+
+https://www.codewars.com/kata/55911ef14065454c75000062/javascript
+*/
+
+//My solution
+function multiply(a, b) {return (BigInt(a) * BigInt(b)).toString()}
