e4a645224ab979a017f052b072752c22b2e00e88
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-03-16 11:27:10 +0200

    The Hashtag Generator solution
    5-kyu solution

---
+++ 5-kyu/The Hashtag Generator/index.js
@@ -0,0 +1,31 @@
+/*
+The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+    It must start with a hashtag (#).
+    All words must have their first letter capitalized.
+    If the final result is longer than 140 chars it must return false.
+    If the input or the result is an empty string it must return false.
+
+Examples
+
+" Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+"    Hello     World   "                  =>  "#HelloWorld"
+""                                        =>  false
+
+https://www.codewars.com/kata/52449b062fb80683ec000024/javascript
+*/
+
+//My solution
+function generateHashtag (str) {
+  let joined = str.split(" ").map(word=> word.charAt(0).toUpperCase() + word.slice(1)).join("");
+  if(joined.length > 139){
+      return false;
+    } else if(joined === ""){
+      return false
+    } else {
+      return `#${joined.trim()}`; 
+    }
+}
