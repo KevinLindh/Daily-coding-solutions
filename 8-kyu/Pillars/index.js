520919e835997790ac61a504732edc99a0564b76
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-03-24 10:06:22 +0200

    Pillars solution
    8-kyu solution

---
+++ 8-kyu/Pillars/index.js
@@ -0,0 +1,19 @@
+/*
+There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+
+    number of pillars (≥ 1);
+    distance between pillars (10 - 30 meters);
+    width of the pillar (10 - 50 centimeters).
+
+Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+
+https://www.codewars.com/kata/5bb0c58f484fcd170700063d
+*/
+
+//My solution
+function pillars(num_pill, dist, width) {
+  if(num_pill < 2){
+    return 0;
+  }
+  return (num_pill-1)*width-width + (num_pill-1)*dist*100
+}
