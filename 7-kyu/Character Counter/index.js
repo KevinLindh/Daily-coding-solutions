221156c9180d081934831a4f3b2be659e23ea002
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-03-25 09:15:30 +0200

    Character Counter solution
    7-kyu solution

---
+++ 7-kyu/Character Counter/index.js
@@ -0,0 +1,30 @@
+/*
+You are going to be given a word. Your job will be to make sure that each character in that word has the exact same number of occurrences. You will return true if it is valid, or false if it is not.
+
+For this kata, capitals are considered the same as lowercase letters. Therefore: "A" == "a"
+
+The input is a string (with no spaces) containing [a-z],[A-Z],[0-9] and common symbols. The length will be 0 < length < 100.
+Examples
+
+    "abcabc" is a valid word because "a" appears twice, "b" appears twice, and"c" appears twice.
+    "abcabcd" is NOT a valid word because "a" appears twice, "b" appears twice, "c" appears twice, but "d" only appears once!
+    "123abc!" is a valid word because all of the characters only appear once in the word.
+
+https://www.codewars.com/kata/56786a687e9a88d1cf00005d/javascript
+*/
+
+//My solution
+function validateWord(s){
+  let repeats = 0;
+  let lowerCase = s.toLowerCase();
+  for(let i = 0; i < s.length; i++){
+    if(lowerCase.indexOf(lowerCase[i]) === lowerCase.lastIndexOf(lowerCase[i])){
+      } else{
+      repeats++;
+    }
+  }
+  if(repeats === 0){
+    return true;
+  }
+  return repeats === s.length
+}
