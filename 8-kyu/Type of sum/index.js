ee47fbc19c5a5e738bf56619ca01ee3de8e2936e
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-04-03 09:48:58 +0300

    Type of sum solution
    8-kyu solution

---
+++ 8-kyu/Type of sum/index.js
@@ -0,0 +1,10 @@
+/*
+Return the type of the sum of the two arguments
+
+https://www.codewars.com/kata/5a2e9ae2b6cfd7692a0000ba/javascript
+*/
+
+//My solution
+function typeOfSum(a, b) {
+  return typeof(a + b);
+}
