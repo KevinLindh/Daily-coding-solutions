aa300fc5ef4e2090756e3e4b03379b90ef6b67ec
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-04-09 10:51:05 +0300

    Classy Extentions solution
    8-kyu solution

---
+++ 8-kyu/Classy Extentions/index.js
@@ -0,0 +1,19 @@
+/*
+Classy Extensions
+Classy Extensions, this kata is mainly aimed at the new JS ES6 Update introducing extends keyword. You will be preloaded with the Animal class, so you should only edit the Cat class.
+Task
+Your task is to complete the Cat class which Extends Animal and replace the speak method to return the cats name + meows. e.g. 'Mr Whiskers meows.'
+
+The name attribute is passed with this.name (JS), @name (Ruby) or self.name (Python).
+
+Reference: JS, Ruby, Python.
+
+https://www.codewars.com/kata/55a14aa4817efe41c20000bc/javascript
+*/
+
+//My solution
+class Cat extends Animal {
+  speak() {
+    return `${this.name} meows.`;
+  }
+}
