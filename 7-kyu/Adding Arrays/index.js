0b4aca27e4831ab236d68892c80de8b264e39d74
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-04-11 09:54:46 +0300

    Adding Arrays solution
    7-kyu solution

---
+++ 7-kyu/Adding Arrays/index.js
@@ -0,0 +1,51 @@
+/*
+Create a function that takes an array of letters, and combines them into words in a sentence.
+
+The array will be formatted as so:
+
+[['J','L','L','M']
+,['u','i','i','a']
+,['s','v','f','n']
+,['t','e','e','']]
+
+The function should combine all the 0th indexed letters to create the word 'just', all the 1st indexed letters to create the word 'live', etc.
+
+Shorter words will have an empty string in the place once the word has already been mapped out (see the last element in the last element in the array).
+
+Examples:
+
+arrAdder([
+['J','L','L','M'],
+['u','i','i','a'],
+['s','v','f','n'],
+['t','e','e','']
+]) // => "Just Live Life Man"
+
+arrAdder([ 
+  [ 'T', 'M', 'i', 't', 'p', 'o', 't', 'c' ],
+  [ 'h', 'i', 's', 'h', 'o', 'f', 'h', 'e' ],
+  [ 'e', 't', '', 'e', 'w', '', 'e', 'l' ],
+  [ '', 'o', '', '', 'e', '', '', 'l' ],
+  [ '', 'c', '', '', 'r', '', '', '' ],
+  [ '', 'h', '', '', 'h', '', '', '' ],
+  [ '', 'o', '', '', 'o', '', '', '' ],
+  [ '', 'n', '', '', 'u', '', '', '' ],
+  [ '', 'd', '', '', 's', '', '', '' ],
+  [ '', 'r', '', '', 'e', '', '', '' ],
+  [ '', 'i', '', '', '', '', '', '' ],
+  [ '', 'a', '', '', '', '', '', '' ] ]) // => "The Mitochondria is the powerhouse of the cell"
+
+https://www.codewars.com/kata/59778cb1b061e877c50000cc/javascript
+*/
+
+//My solution
+function arrAdder(arr) {
+  let newArr = [];
+  for(let i = 0; i < arr[0].length; i++){
+    for(let j = 0; j < arr.length; j++){
+      newArr.push(arr[j][i]);
+     }
+    newArr.push(" ");
+    }
+  return newArr.join("").trim();
+}
