4d618443189146770c42c92825561cf7b6945eb9
KevinLindh <94518833+KevinLindh@users.noreply.github.com>
2022-04-17 22:15:14 +0300

    Filter unused digits solution
    7-kyu solution

---
+++ 7-kyu/Filter unused digits/index.js
@@ -0,0 +1,29 @@
+/*
+Given a varying number of integer arguments, return the digits that are not present in any of them.
+
+Example:
+
+[12, 34, 56, 78]  =>  "09"
+[2015, 8, 26]     =>  "3479"
+
+Note: the digits in the resulting string should be sorted.
+
+https://www.codewars.com/kata/55de6173a8fbe814ee000061
+*/
+
+//My solution
+function unusedDigits() {
+  let newArr = [];
+  let numbers = "0123456789";
+  let answer = [];
+  for(let i=0; i < arguments.length; i++){
+    newArr.push(arguments[i]);
+  }
+  let string = newArr.join("")
+  for (let j=0; j < numbers.length; j++){
+   if(string.indexOf(numbers[j]) === -1){
+     answer.push(numbers[j]);
+   }
+  }
+  return answer.join("");
+}
