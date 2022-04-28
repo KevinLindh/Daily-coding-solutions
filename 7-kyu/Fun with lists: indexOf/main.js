306d4fe1fc1a94f2d8d99b384af4ff5d554ec6fc
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-04-28 10:35:15 +0300

    Fun with lists: indexOf solution
    7-kyu solution

---
+++ 7-kyu/Fun with lists: indexOf/main.js
@@ -0,0 +1,35 @@
+/*
+Implement the method indexOf (index_of in PHP), which accepts a linked list (head) and a value, and returns the index (zero based) of the first occurrence of that value if exists, or -1 otherwise.
+
+For example: Given the list: 1 -> 2 -> 3 -> 3, and the value 3, indexOf / index_of should return 2.
+
+The linked list is defined as follows:
+
+function Node(data, next = null) {
+  this.data = data;
+  this.next = next;
+}
+
+Note: the list may be null and can hold any type of value.
+
+Good luck!
+
+https://www.codewars.com/kata/581c6b075cfa83852700021f
+*/
+
+//My solution
+function indexOf(head, value) {
+  if(head === null){
+   return -1; 
+  } else {
+    let index = 0;
+    while(head !== null){
+      if(head.data === value){
+        return index;
+      }
+      index++;
+      head = head.next;
+    }
+    return -1;
+  }
+}
