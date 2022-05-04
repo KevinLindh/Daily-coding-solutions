342751a7e26539920f0388c38e4e1c5711c4919e
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-05-04 09:51:36 +0300

    Complete The Pattern #5 - Even Ladder solution
    7-kyu solution

---
+++ 7-kyu/Complete The Pattern #5 - Even Ladder/main.js
@@ -0,0 +1,46 @@
+/*
+Task:
+
+You have to write a function pattern which creates the following pattern up to n/2 number of lines.
+
+    If n <= 1 then it should return "" (i.e. empty string).
+    If any odd number is passed as argument then the pattern should last up to the largest even number which is smaller than the passed odd number.
+
+Examples:
+
+pattern(8):
+
+22
+4444
+666666
+88888888
+
+pattern(5):
+
+22
+4444
+
+Note: There are no spaces in the pattern
+
+Hint: Use \n in string to jump to next line
+
+https://www.codewars.com/kata/55749101ae1cf7673800003e/javascript
+*/
+
+// My solution
+function pattern(n){
+ let output="";
+  if(n <= 1){
+    return "";
+  }
+  if(n%2 !== 0){
+    n -= 1;
+  }
+  for(let i=1; i<n; i++){
+    if(i % 2 === 0){
+      output += new Array(i).fill(i, 0).join("") + "\n";
+    }
+  }
+  output += new Array(n).fill(n, 0).join("");
+ return output;
+}
