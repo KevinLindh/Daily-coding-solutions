7cf33c4ce6d12e1a60d0b00a9060f55fef79d4e6
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-05-06 11:03:40 +0300

    Separating Strings solution
    6-kyu solution

---
+++ 6-kyu/Separating Strings/main.js
@@ -0,0 +1,58 @@
+/*
+Create a function that takes a string and separates it into a sequence of letters.
+
+The array will be formatted as so:
+
+[['J','L','L','M']
+,['u','i','i','a']
+,['s','v','f','n']
+,['t','e','e','']]
+
+The function should separate each word into individual letters, with the first word in the sentence having its letters in the 0th index of each 2nd dimension array, and so on.
+
+Shorter words will have an empty string in the place once the word has already been mapped out. (See the last element in the last part of the array.)
+
+Examples:
+
+sepStr("Just Live Life Man")
+// => [['J','L','L','M'],
+// => ['u','i','i','a'],
+// => ['s','v','f','n'],
+// => ['t','e','e','']]);
+
+sepStr("The Mitochondria is the powerhouse of the cell")
+// => [ [ 'T', 'M', 'i', 't', 'p', 'o', 't', 'c' ],
+// => [ 'h', 'i', 's', 'h', 'o', 'f', 'h', 'e' ],
+// => [ 'e', 't', '', 'e', 'w', '', 'e', 'l' ],
+// => [ '', 'o', '', '', 'e', '', '', 'l' ],
+// => [ '', 'c', '', '', 'r', '', '', '' ],
+// => [ '', 'h', '', '', 'h', '', '', '' ],
+// => [ '', 'o', '', '', 'o', '', '', '' ],
+// => [ '', 'n', '', '', 'u', '', '', '' ],
+// => [ '', 'd', '', '', 's', '', '', '' ],
+// => [ '', 'r', '', '', 'e', '', '', '' ],
+// => [ '', 'i', '', '', '', '', '', '' ],
+// => [ '', 'a', '', '', '', '', '', '' ]]
+
+https://www.codewars.com/kata/5977ef1f945d45158d00011f/javascript
+*/
+
+//My solution
+function sepStr(str) {
+  let newArr = str.split(" ");
+  let longest = str.split(" ").map(val => val.length);
+  let nested = Math.max(...longest);
+  let answer = [];
+  for(let i = 0; i < nested; i++){
+    let secondArr = [];
+    for(let j = 0; j < newArr.length; j++){
+      if(newArr[j][i] === undefined){
+        secondArr.push("")
+      } else {
+        secondArr.push(newArr[j][i])
+      }
+    }
+    answer.push(secondArr);
+  }
+  return answer;
+}
