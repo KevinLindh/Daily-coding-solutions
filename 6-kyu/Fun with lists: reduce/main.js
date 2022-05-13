bfb61603b392e55d754e2f96e04df8e160eb37f7
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-05-13 14:56:01 +0300

    Fun with lists: reduce solution
    6-kyu solution

---
+++ 6-kyu/Fun with lists: reduce/main.js
@@ -0,0 +1,48 @@
+/*
+Implement the method reduce, which accepts three arguments:
+
+    linked list (head)
+    bi-function - (accumulated_value, current_element_data)
+    initial value
+
+This method should return the result of applying the given function on every element with the accumulating result, starting with the initial value.
+
+For example:
+
+Given the list: 1 -> 2 -> 3, the function (acc, curr) => acc + curr and an initial value of 0, reduce should return 6, because:
+
+    (0, 1) and the function (acc, curr) => acc + curr gives 1
+    (1, 2) and the function (acc, curr) => acc + curr gives 3
+    (3, 3) and the function (acc, curr) => acc + curr gives 6
+
+Another example:
+
+Given the list: 1 -> 2 -> 3 -> 4, the function (acc, curr) => acc * curr and an initial value of 1, reduce should return 24
+
+The linked list is defined as follows:
+
+function Node(data, next = null) {
+  this.data = data;
+  this.next = next;
+}
+
+Note: the list may be null and can hold any type of value.
+
+Good luck!
+
+https://www.codewars.com/kata/58319f37aeb69a89a00000c7
+*/
+
+//My solution
+function reduce(head, f, init) {
+ if(head === null){
+   return init;
+ } else{
+   let total = [];
+   while(head){
+     total.push(head.data);
+     head = head.next;
+   }
+  return total.reduce(f, init);
+ }
+}
