6cbe190c2b03b61fefe2cb241d7098125ec5bc9b
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-05-18 10:15:34 +0300

    Incrementer solution
    7-kyu solution

---
+++ 7-kyu/Incrementer/main.js
@@ -0,0 +1,23 @@
+/*
+Given an input of an array of digits, return the array with each digit incremented by its position in the array: the first digit will be incremented by 1, the second digit by 2, etc. Make sure to start counting your positions from 1 ( and not 0 ).
+
+Your result can only contain single digit numbers, so if adding a digit with its position gives you a multiple-digit number, only the last digit of the number should be returned.
+Notes:
+
+    return an empty array if your array is empty
+    arrays will only contain numbers so don't worry about checking that
+
+Examples:
+
+[1, 2, 3]  -->  [2, 4, 6]   #  [1+1, 2+2, 3+3]
+
+[4, 6, 9, 1, 3]  -->  [5, 8, 2, 5, 8]  #  [4+1, 6+2, 9+3, 1+4, 3+5]
+                                       #  9+3 = 12  -->  2
+                                       
+https://www.codewars.com/kata/590e03aef55cab099a0002e8/javascript
+*/
+
+//My solution
+function incrementer(nums) { 
+  return nums.map((val, i)=> (val+i+1).toString().length < 2 ? (val+i+1) : Number((val+i+1).toString().split("").splice(-1)));
+}
