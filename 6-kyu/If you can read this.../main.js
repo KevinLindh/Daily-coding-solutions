dd627b1d2be5e7a5f704eb2d516d5eb7b4c2067f
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-05-31 11:57:32 +0300

    If you can read this... solution
    6-kyu solution

---
+++ 6-kyu/If you can read this.../main.js
@@ -0,0 +1,43 @@
+/*
+The idea for this kata came from 9gag today:
+
+The lyrics to "Never gonna give you up" by Rick Astley encoded in the NATO phonetic alphabet
+Task
+
+You'll have to translate a string to Pilot's alphabet (NATO phonetic alphabet).
+
+Input:
+
+If, you can read?
+
+Output:
+
+India Foxtrot , Yankee Oscar Uniform Charlie Alfa November Romeo Echo Alfa Delta ?
+
+Note:
+
+    There are preloaded dictionary you can use, named NATO
+    The set of used punctuation is ,.!?.
+    Punctuation should be kept in your return string, but spaces should not.
+    Xray should not have a dash within.
+    Every word and punctuation mark should be seperated by a space ' '.
+    There should be no trailing whitespace
+
+https://www.codewars.com/kata/586538146b56991861000293/javascript
+*/
+
+// My solution
+function to_nato(words) {
+  let answer = ""
+  let lower = words.toLowerCase();
+  let punctuations = [",", ".", "!", "?"];
+  for(let i = 0; i < words.length; i++){
+    if(punctuations.indexOf(lower[i])!== -1){
+      answer += lower[i] + " ";
+    } else if(lower[i].toLowerCase() === lower[i].toUpperCase()){
+    } else{
+      answer += NATO[lower[i]] + " ";
+    }
+  }
+  return answer.trim();
+}
