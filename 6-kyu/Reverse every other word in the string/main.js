434d5d1c134922acc2fd86a6bbbfeef4b08b3f2f
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-08 15:35:06 +0300

    Reverse every other word in the string solution
    6-kyu solution

---
+++ 6-kyu/Reverse every other word in the string/main.js
@@ -0,0 +1,17 @@
+/*
+Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, 
+while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+
+https://www.codewars.com/kata/58d76854024c72c3e20000de
+*/
+
+//My solution
+function reverse(str){
+  let newArr = str.trim().split(" ");
+  for(let i = 0; i < newArr.length; i++){
+    if(i%2 === 1){
+      newArr[i] = newArr[i].split("").reverse().join("");
+    }
+  }
+  return newArr.join(" ");
+}
