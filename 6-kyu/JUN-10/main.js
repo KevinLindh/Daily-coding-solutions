40a44d9da9352673c1374e27f2fe9a0f4e512fef
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-10 09:28:13 +0300

    JUN-10

--- 6-kyu/JUN-10/main.js
+++ 6-kyu/JUN-10/main.js
@@ -1,7 +1,7 @@
 /*
 The objective is to return all pairs of integers from a given array of integers that have a difference of 2.
 
-The result array should be sorted in ascending order of values.
+The result array should be sorted in ascending order of values. 
 
 Assume there are no duplicate integers in the array. The order of the integers in the input array should not matter.
 Examples
