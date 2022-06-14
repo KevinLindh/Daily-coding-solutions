14542dbc7092ce63387ee33dfbbc7f9bc3bae583
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 14:40:01 +0300

    Update index.js

--- 8-kyu/APR-13/index.js
+++ 8-kyu/APR-13/index.js
@@ -2,8 +2,6 @@
 Get ASCII value of a character.
  
 For the ASCII table you can refer to http://www.asciitable.com/
-
-https://www.codewars.com/kata/55acfc59c3c23d230f00006d/javascript
 */
 
 //My solution
