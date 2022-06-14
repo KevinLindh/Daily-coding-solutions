9b369302fa3072769d7b1d7f01e56704590f09ab
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 14:08:28 +0300

    FEB-26

--- 8-kyu/FEB-26-2/index .js
+++ 8-kyu/FEB-26-2/index .js
@@ -2,8 +2,6 @@
 Implement a function which convert the given boolean value into its string representation.
 
 Note: Only valid inputs will be given.
-
-https://www.codewars.com/kata/551b4501ac0447318f0009cd/javascript
 */
 
 //My solution
