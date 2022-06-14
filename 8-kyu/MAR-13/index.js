b6f8e77ab819b8b948f1fe85d0c0a1febba10ada
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 13:13:00 +0300

    MAR-13

--- 8-kyu/MAR-13/index.js
+++ 8-kyu/MAR-13/index.js
@@ -6,9 +6,6 @@ Examples:
 2) n =  12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
 3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
 4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
-
-
-https://www.codewars.com/kata/5545f109004975ea66000086/solutions/javascript
 */
 
 //My solution
