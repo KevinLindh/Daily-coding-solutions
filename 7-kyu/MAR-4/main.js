381e12bf0db7bd0e26fc3e3d5ca822a14bcf03a2
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 15:45:14 +0300

    MAR-4

--- 7-kyu/MAR-4/main.js
+++ 7-kyu/MAR-4/main.js
@@ -8,8 +8,6 @@ For example:
 findSum(5) should return 8 (3 + 5)
 
 findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
-
-https://www.codewars.com/kata/57f36495c0bb25ecf50000e7/javascript
 */
 
 // My solution
