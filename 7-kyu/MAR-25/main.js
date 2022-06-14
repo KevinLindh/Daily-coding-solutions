9a59ae8dc852ff8aaa0eecc9bca78ca62b50a6ac
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 15:30:03 +0300

    MAR-25

--- 7-kyu/MAR-25/main.js
+++ 7-kyu/MAR-25/main.js
@@ -9,8 +9,6 @@ Examples
     "abcabc" is a valid word because "a" appears twice, "b" appears twice, and"c" appears twice.
     "abcabcd" is NOT a valid word because "a" appears twice, "b" appears twice, "c" appears twice, but "d" only appears once!
     "123abc!" is a valid word because all of the characters only appear once in the word.
-
-https://www.codewars.com/kata/56786a687e9a88d1cf00005d/javascript
 */
 
 //My solution
