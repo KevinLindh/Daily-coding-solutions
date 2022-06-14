12e0e83dab71ad7e9bd3c5deacdfd6092b2486fe
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 15:01:56 +0300

    Update index.js

--- 6-kyu/FEB-3/index.js
+++ 6-kyu/FEB-3/index.js
@@ -6,8 +6,6 @@ Example
 "camelCasing"  =>  "camel Casing"
 "identifier"   =>  "identifier"
 ""             =>  ""
-
-https://www.codewars.com/kata/5208f99aee097e6552000148/javascript
 */
 
 // My solution
