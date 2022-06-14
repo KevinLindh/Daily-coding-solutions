d650d19c72c5e1c085dd834f00b555b5632b90c4
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 15:14:01 +0300

    Update index.js

--- 6-kyu/MAR-14/index.js
+++ 6-kyu/MAR-14/index.js
@@ -2,8 +2,6 @@
 Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
 
 Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
-
-https://www.codewars.com/kata/526571aae218b8ee490006f4/javascript
 */
 
 // My solution
