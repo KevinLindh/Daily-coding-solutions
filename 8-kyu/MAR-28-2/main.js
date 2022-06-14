2e19c2b1fc38caf6da06d9a19e8334bdbdadacd2
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 13:38:07 +0300

    MAR-28

--- 8-kyu/MAR-28-2/main.js
+++ 8-kyu/MAR-28-2/main.js
@@ -6,8 +6,6 @@ You will be given the high street price (normPrice), the duty free discount (dis
 For example, if a bottle cost £10 normally and the discount in duty free was 10%, you would save £1 per bottle. If your holiday cost £500, the answer you should return would be 500.
 
 All inputs will be integers. Please return an integer. Round down.
-
-https://www.codewars.com/kata/57e92e91b63b6cbac20001e5/javascript
 */
 
 //My solution
