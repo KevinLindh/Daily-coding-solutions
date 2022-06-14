7936c671cc776081c9e78bcf57e9c7788f27c950
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 13:58:35 +0300

    MAR-8

--- 8-kyu/MAR-8-2/main.js
+++ 8-kyu/MAR-8-2/main.js
@@ -7,8 +7,6 @@ If the input is an empty array or is null, return an empty array.
 Example
 
 For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
-
-https://www.codewars.com/kata/576bb71bbbcf0951d5000044/javascript
 */
 
 
