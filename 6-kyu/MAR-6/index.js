d8d46cfae49fc8ce80e7ebdb33b5cb8a6be725c0
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 15:13:05 +0300

    Update index.js

--- 6-kyu/MAR-6/index.js
+++ 6-kyu/MAR-6/index.js
@@ -11,8 +11,6 @@ Example
 alphabetPosition("The sunset sets at twelve o' clock.")
 
 Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
-
-https://www.codewars.com/kata/546f922b54af40e1e90001da/javascript
 */
 
 // My solution
