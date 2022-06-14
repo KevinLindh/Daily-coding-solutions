d1b730f5edf5fa8add67245b74c3bf4e76e8109f
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 15:42:58 +0300

    FEB-13

--- 7-kyu/FEB-13/main.js
+++ 7-kyu/FEB-13/main.js
@@ -4,8 +4,6 @@ Welcome. In this kata, you are asked to square every digit of a number and conca
 For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
 
 Note: The function accepts an integer and returns an integer
-
-https://www.codewars.com/kata/546e2562b03326a88e000020/javascript
 */
 
 // My solution
