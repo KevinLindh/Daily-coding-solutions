4d8255a85687da84d41d611b625e4da789bfd18a
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 11:47:14 +0300

    MAR-26

--- 7-kyu/MAR-26/index.js
+++ 7-kyu/MAR-26/index.js
@@ -4,8 +4,6 @@ Return the number (count) of vowels in the given string.
 We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
 The input string will only consist of lower case letters and/or spaces.
-
-https://www.codewars.com/kata/54ff3102c1bad923760001f3
 */
 
 //My solution
