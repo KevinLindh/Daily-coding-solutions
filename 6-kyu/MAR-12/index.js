07d79a24f60c2a2febd9104d513a8c91e2a4282c
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 15:14:41 +0300

    Update index.js

--- 6-kyu/MAR-12/index.js
+++ 6-kyu/MAR-12/index.js
@@ -6,8 +6,6 @@ Finish the solution so that it returns the sum of all the multiples of 3 or 5 be
 Note: If the number is a multiple of both 3 and 5, only count it once.
 
 Courtesy of projecteuler.net (Problem 1)
-
-https://www.codewars.com/kata/514b92a657cdc65150000006/javascript
 */
 
 //My solution
