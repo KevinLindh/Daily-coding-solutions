08fe4d8bd4cfab2d267638798de7d06d6a88bf42
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 15:44:17 +0300

    Update main.js

--- 7-kyu/FEB-1/main.js
+++ 7-kyu/FEB-1/main.js
@@ -1,9 +1,4 @@
 /*
-
-Title: 
-  "String ends with?"
-
-  Description:
     Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string). 
     
   Examples: 
