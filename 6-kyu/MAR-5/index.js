db8fed6133c8ce0eff45db028f6a300a8a351bc8
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 15:12:28 +0300

    Update index.js

--- 6-kyu/MAR-5/index.js
+++ 6-kyu/MAR-5/index.js
@@ -20,8 +20,6 @@ countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
 Note
 
 In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.
-
-https://www.codewars.com/kata/583203e6eb35d7980400002a/javascript
 */
 
 // My solution
