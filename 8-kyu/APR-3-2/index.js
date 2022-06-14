6b05f7792941f8fa2d0dca88e49b15d8770803ed
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 12:11:38 +0300

    APR-3

--- 8-kyu/APR-3-2/index.js
+++ 8-kyu/APR-3-2/index.js
@@ -10,8 +10,6 @@ Notes:
     The movement of the petal is quite complicated, so in this case we can see the velocity as a constant during its falling.
     Pay attention to the data types.
     If the initial velocity is non-positive, the return value should be 0
-
-https://www.codewars.com/kata/5a0be7ea8ba914fc9c00006b/javascript
 */
 
 //My solution
