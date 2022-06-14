0632bb8d793b7031434407c072fe43bd29ab66f9
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 15:44:07 +0300

    FEB-1

--- 7-kyu/FEB-1/main.js
+++ 7-kyu/FEB-1/main.js
@@ -8,11 +8,7 @@ Title:
     
   Examples: 
     solution('abc', 'bc') // returns true
-    solution('abc', 'd') // returns false
-    
-   Kata Link:
-   https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/javascript
-    
+    solution('abc', 'd') // returns false   
 */
 
 //My Solution
