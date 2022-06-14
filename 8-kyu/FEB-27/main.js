f98958c9e24151c7874a76a067e05c801c5f36e7
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 14:31:23 +0300

    FEB-27

--- 8-kyu/FEB-27/main.js
+++ 8-kyu/FEB-27/main.js
@@ -2,8 +2,6 @@
 Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
 
 [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
-
-https://www.codewars.com/kata/57f780909f7e8e3183000078/javascript
 */
 
 // My solution
