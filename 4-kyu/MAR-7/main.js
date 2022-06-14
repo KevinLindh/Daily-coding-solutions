d359b2d5fa68931993c4c17f82471127449bef7e
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 13:01:25 +0300

    Update main.js

--- 4-kyu/MAR-7/main.js
+++ 4-kyu/MAR-7/main.js
@@ -6,8 +6,6 @@ For example:
 sumStrings('1','2') // => '3'
 
 A string representation of an integer will contain no characters besides the ten numerals "0" to "9".
-
-https://www.codewars.com/kata/5324945e2ece5e1f32000370/javascript
 */
 
 //My solution
