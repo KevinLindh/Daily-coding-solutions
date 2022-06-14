1e6605aec730d1d923720880d8ac8a4c287a5cb3
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 12:24:50 +0300

    Update index.js

--- 8-kyu/MAR-15/index.js
+++ 8-kyu/MAR-15/index.js
@@ -4,8 +4,6 @@ Complete the solution so that it reverses all of the words within the string pas
 Example:
 
 "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
-
-https://www.codewars.com/kata/51c8991dee245d7ddf00000e/javascript
 */
 
 // My solution
