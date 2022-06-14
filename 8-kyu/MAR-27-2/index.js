77861bd2467a19c78588a56e5f513f06de288839
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 14:46:16 +0300

    Update index.js

--- 8-kyu/MAR-27-2/index.js
+++ 8-kyu/MAR-27-2/index.js
@@ -6,8 +6,6 @@ AddExtra method adds a new item to the list and returns the list. The new item c
 In our test case we check to assure that the returned list has one more item than the input list. However the method needs some modification to pass this test.
 
 P.S. You have to create a new list and add a new item to that. (This Kata is originally designed for C# language and it shows that adding a new item to the input list is not going to work, even though the parameter is passed by value, but the value is poining to the reference of list and any change on parameter will be seen by caller)
-
-https://www.codewars.com/kata/566dc05f855b36a031000048/javascript
 */
 
 //My solution
