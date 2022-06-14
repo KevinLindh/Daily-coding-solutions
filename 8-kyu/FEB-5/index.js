b13418537776d82540175d2abd260b7107ee8371
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 12:41:07 +0300

    FEB-5

--- 8-kyu/FEB-5/index.js
+++ 8-kyu/FEB-5/index.js
@@ -1,7 +1,5 @@
 /*
 Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
-
-https://www.codewars.com/kata/57a0885cbb9944e24c00008e/javascript
 */
 
 // My solution
