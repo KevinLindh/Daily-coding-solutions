796b6f49bbed6df3dcc05df0f0da140088600dbc
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 12:45:57 +0300

    APR-5

--- 8-kyu/APR-5/index.js
+++ 8-kyu/APR-5/index.js
@@ -2,8 +2,6 @@
 Write a function that returns a string in which firstname is swapped with last name.
 
 nameShuffler('john McClane'); => "McClane john"
-
-https://www.codewars.com/kata/559ac78160f0be07c200005a/solutions/javascript
 */
 
 //My solution
