f124e9a70732e46bc84580a80de8532632c615da
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 15:02:21 +0300

    Update index.js

--- 6-kyu/FEB-7/index.js
+++ 6-kyu/FEB-7/index.js
@@ -2,8 +2,6 @@
 The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
 
 What if the string is empty? Then the result should be empty object literal, {}.
-
-https://www.codewars.com/kata/52efefcbcdf57161d4000091/javascript
 */
 
 // My solution
