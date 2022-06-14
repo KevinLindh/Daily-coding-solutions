dff25d1a92cff7530212c963b360c52dcbc1db9d
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 13:59:26 +0300

    MAR-16

--- 8-kyu/MAR-16-2/main.js
+++ 8-kyu/MAR-16-2/main.js
@@ -5,8 +5,6 @@ oddCount(7) //=> 3, i.e [1, 3, 5]
 oddCount(15) //=> 7, i.e [1, 3, 5, 7, 9, 11, 13]
 
 Expect large Inputs!
-
-https://www.codewars.com/kata/59342039eb450e39970000a6/javascript
 */
 
 //My solution
