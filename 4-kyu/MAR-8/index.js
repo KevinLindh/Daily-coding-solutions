f47f2c1b95399c6be3c84a741cb1a569474c3220
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 13:01:40 +0300

    Update index.js

--- 4-kyu/MAR-8/index.js
+++ 4-kyu/MAR-8/index.js
@@ -12,8 +12,6 @@ solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 1
 // returns "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
 
 Courtesy of rosettacode.org
-
-https://www.codewars.com/kata/51ba717bb08c1cd60f00002f/javascript
 */
 
 //My solution
