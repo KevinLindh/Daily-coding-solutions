37d37437c42a4675ddc6c932e10cdff5bb6ce1c6
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 13:03:59 +0300

    Update index.js

--- 5-kyu/MAR-12-2/index.js
+++ 5-kyu/MAR-12-2/index.js
@@ -2,8 +2,6 @@
 Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
 
 moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
-
-https://www.codewars.com/kata/52597aa56021e91c93000cb0/javascript
 */
 
 //My solution
