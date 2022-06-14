146e337ee04c1c61323df28d4f66d3056502f8d5
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 14:53:03 +0300

    Update index.js

--- 6-kyu/APR-7/index.js
+++ 6-kyu/APR-7/index.js
@@ -8,8 +8,6 @@ for example, when given size is 3:
 3 6 9
 
 for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
-
-https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08/javascript
 */
 
 //My solution
