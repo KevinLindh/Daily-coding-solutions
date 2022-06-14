841f60c28a137b30d117b399af0917d2e1174044
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 11:58:03 +0300

    FEB-12

--- 8-kyu/FEB-12/index.js
+++ 8-kyu/FEB-12/index.js
@@ -6,8 +6,6 @@ You will need a rental car in order for you to get around in your vacation. The 
 Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
 
 Write a code that gives out the total amount for different days(d).
-
-https://www.codewars.com/kata/568d0dd208ee69389d000016/javascript
 */
 
 //My solution
