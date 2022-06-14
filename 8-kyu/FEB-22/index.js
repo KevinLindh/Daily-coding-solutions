1e32e9e55c2cf89bad90c89f4f84e69fae936704
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 13:39:44 +0300

    FEB-22

--- 8-kyu/FEB-22/index.js
+++ 8-kyu/FEB-22/index.js
@@ -6,8 +6,6 @@ Use conditionals to return the proper message:
 case	return
 name equals owner	'Hello boss'
 otherwise	'Hello guest'
-
-https://www.codewars.com/kata/5772da22b89313a4d50012f7/javascript
 */
 
 //My solution
