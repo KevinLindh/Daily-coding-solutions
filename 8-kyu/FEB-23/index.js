74b8364558fa8fb6db8d65600c84d06aca5a977a
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 13:08:59 +0300

    FEB-23

--- 8-kyu/FEB-23/index.js
+++ 8-kyu/FEB-23/index.js
@@ -7,8 +7,6 @@ setAlarm(true, true) -> false
 setAlarm(false, true) -> false
 setAlarm(false, false) -> false
 setAlarm(true, false) -> true
-
-https://www.codewars.com/kata/568dcc3c7f12767a62000038/javascript
 */
 
 // My solution
