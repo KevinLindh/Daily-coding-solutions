876b75a7d683f40477d2233dc078f75bbc3c0cb9
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 15:29:25 +0300

    APR-19

--- 7-kyu/APR-19/main.js
+++ 7-kyu/APR-19/main.js
@@ -12,8 +12,6 @@ Example
 
 input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
 output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
-
-https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa/javascript
 */
 
 //My solution
