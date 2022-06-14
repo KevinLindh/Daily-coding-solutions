8fa4bc9bbd4e4f79d4fbafee89449e59f5bb27c9
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 13:40:30 +0300

    FEB-19

--- 8-kyu/FEB-19/index.js
+++ 8-kyu/FEB-19/index.js
@@ -10,8 +10,6 @@ Numerical Score 	Letter Grade
 0 <= score < 60 	'F'
 
 Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
-
-https://www.codewars.com/kata/55cbd4ba903825f7970000f5/javascript
 */
 
 //My solution
