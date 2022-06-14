9d39132849db7fe8f662983838b39fde47ef9257
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 15:02:10 +0300

    Update index.js

--- 6-kyu/FEB-4/index.js
+++ 6-kyu/FEB-4/index.js
@@ -6,9 +6,6 @@ For example:
 uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 uniqueInOrder([1,2,2,3,3])       == [1,2,3]
-
-https://www.codewars.com/kata/54e6533c92449cc251001667/javascript
-
 */
 
 // My solution
