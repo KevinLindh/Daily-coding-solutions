5e93f9b25377be8626ca9de4d1dd5d15b6b0c925
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 11:47:56 +0300

    MAR-20

--- 7-kyu/MAR-20/index.js
+++ 7-kyu/MAR-20/index.js
@@ -14,8 +14,6 @@ minMax([1])           == [1, 1]
 Remarks
 
 All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
-
-https://www.codewars.com/kata/559590633066759614000063
 */
 
 // My solution
