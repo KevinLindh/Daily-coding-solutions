ece93b45985b741dc3e82e8705fba2dd98b6e780
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 15:09:36 +0300

    Update index.js

--- 6-kyu/MAR-16/index.js
+++ 6-kyu/MAR-16/index.js
@@ -14,8 +14,6 @@ The Challenge:
 Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10. This may be True and False in your language, e.g. PHP.
 
 Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.
-
-https://www.codewars.com/kata/5287e858c6b5a9678200083c/javascript
 */
 
 //My solution
