af30fbaaa245527306d0f81b6aa70b915a346204
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 12:12:27 +0300

    FEB-6

--- 8-kyu/FEB-6/index.js
+++ 8-kyu/FEB-6/index.js
@@ -4,8 +4,6 @@ All of the animals are having a feast! Each animal is bringing one dish. There i
 Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
 
 Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
-
-https://www.codewars.com/kata/5aa736a455f906981800360d/javascript
 */
 
 // My solution
