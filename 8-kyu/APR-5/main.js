9a50bcc663aa00e5beed3fd9c93da487c0da1735
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 13:52:26 +0300

    APR-5

--- 8-kyu/APR-5/main.js
+++ 8-kyu/APR-5/main.js
@@ -1,7 +1,5 @@
 /*
 Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
-
-https://www.codewars.com/kata/545991b4cbae2a5fda000158/javascript
 */
 
 //My solution
