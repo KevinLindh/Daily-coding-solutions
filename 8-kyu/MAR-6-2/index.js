0dcbc61757a47a4155f9f45b6c519b6bade63939
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 13:15:03 +0300

    MAR-6

--- 8-kyu/MAR-6-2/index.js
+++ 8-kyu/MAR-6-2/index.js
@@ -6,8 +6,6 @@ invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
 invert([]) == []
 
 You can assume that all values are integers. Do not mutate the input array/list.
-
-https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad/javascript
 */
 
 // My solution
