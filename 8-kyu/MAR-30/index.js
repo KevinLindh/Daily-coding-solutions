69853fddcbf27e9ef3a05182b69f806809e8c7d2
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 14:48:26 +0300

    Update index.js

--- 8-kyu/MAR-30/index.js
+++ 8-kyu/MAR-30/index.js
@@ -1,8 +1,6 @@
 /*
 Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
 Algorithms
-
-https://www.codewars.com/kata/5b68c7029756802aa2000176
 */
 
 //My solution
