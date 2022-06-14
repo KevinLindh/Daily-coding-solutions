49ce61f869803ab01b7a23b4efdf6a43939293e9
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 14:33:30 +0300

    FEB-28

--- 8-kyu/FEB-28-2/index.js
+++ 8-kyu/FEB-28-2/index.js
@@ -8,8 +8,6 @@ if bmi <= 25.0 return "Normal"
 if bmi <= 30.0 return "Overweight"
 
 if bmi > 30 return "Obese"
-
-https://www.codewars.com/kata/57a429e253ba3381850000fb/javascript
 */
 
 // My solution
