adea13f1e0b51622df8d27223e5d002f95e5cb67
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 13:38:44 +0300

    MAR-23

--- 8-kyu/MAR-23/main.js
+++ 8-kyu/MAR-23/main.js
@@ -20,8 +20,6 @@ Examples (input -> output:
 
 * 'R', [3, 2, 1, 2]      ->  [1, 2, 2, 3]
 * 'L', [1, 4, 5, 3, 5 ]  ->  [5, 5, 4, 3, 1]
-
-https://www.codewars.com/kata/5f70c883e10f9e0001c89673/javascript
 */
 
 //My solution
