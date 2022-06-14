34b634b21acf1ea18b04437b96363100a58f5eea
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 15:52:13 +0300

    FEB-21

--- 7-kyu/FEB-21/main.js
+++ 7-kyu/FEB-21/main.js
@@ -10,8 +10,6 @@ Take a look on the test cases.
 Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the return integer can't be negative.
 
 The second value in the first integer array is 0, since the bus is empty in the first bus stop.
-
-https://www.codewars.com/kata/5648b12ce68d9daa6b000099/javascript
 */
 
 //My solution
