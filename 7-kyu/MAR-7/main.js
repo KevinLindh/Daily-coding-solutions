cb336bd626787ab43e339202273afb23f8b3d493
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 15:33:37 +0300

    MAR-7

--- 7-kyu/MAR-7/main.js
+++ 7-kyu/MAR-7/main.js
@@ -4,8 +4,6 @@ In mathematics, the factorial of a non-negative integer n, denoted by n!, is the
 Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#) or IllegalArgumentException (Java) or RangeException (PHP) or throw a RangeError (JavaScript) or ValueError (Python) or return -1 (C).
 
 More details about factorial can be found here.
-
-https://www.codewars.com/kata/54ff0d1f355cfd20e60001fc/javascript
 */
 
 // My solution
