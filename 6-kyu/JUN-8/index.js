8462d17c2d6bf49ea6b547f24f77408cb55305cf
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 15:16:03 +0300

    Update index.js

--- 6-kyu/JUN-8/index.js
+++ 6-kyu/JUN-8/index.js
@@ -1,8 +1,6 @@
 /*
 Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, 
 while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
-
-https://www.codewars.com/kata/58d76854024c72c3e20000de
 */
 
 //My solution
