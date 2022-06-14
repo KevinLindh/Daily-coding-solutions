fad351875d471b3e7883d56baa0d0e6fd221b23d
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-14 14:06:08 +0300

    FEB-4

--- 8-kyu/FEB-4/main.js
+++ 8-kyu/FEB-4/main.js
@@ -4,8 +4,6 @@ Write a function to split a string and convert it into an array of words. For ex
 "Robin Singh" ==> ["Robin", "Singh"]
 
 "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
-
-https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
 */
 
 // My solution
