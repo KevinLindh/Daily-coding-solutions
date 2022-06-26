955812fd5c9709ac65b9d49de4a8c702a4242c9f
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-06-26 13:51:54 +0300

    JUN-26

---
+++ 7-kyu/JUN-26/main.js
@@ -0,0 +1,32 @@
+/*
+Story
+
+YouTube had a like and a dislike button, which allowed users to express their opinions about particular content. It was set up in such a way that you cannot like and dislike a video at the same time. There are two other interesting rules to be noted about the interface: Pressing a button, which is already active, will undo your press. If you press the like button after pressing the dislike button, the like button overwrites the previous "Dislike" state. The same is true for the other way round.
+Task
+
+Create a function that takes in a list of button inputs and returns the final state.
+Examples
+
+likeOrDislike([Dislike]) => Dislike
+likeOrDislike([Like,Like]) => Nothing
+likeOrDislike([Dislike,Like]) => Like
+likeOrDislike([Like,Dislike,Dislike]) => Nothing
+
+Notes
+
+    If no button is currently active, return Nothing.
+    If the list is empty, return Nothing.
+*/
+
+//My solution
+function likeOrDislike(buttons) {
+  let answer = 'Nothing';
+  for(let i = 0; i < buttons.length; i++){
+    if(buttons[i] != answer){
+      answer = buttons[i];
+    } else{
+      answer = 'Nothing'
+    }
+  }
+  return answer;
+}
