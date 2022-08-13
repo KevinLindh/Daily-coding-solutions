51a91b1638903646dd54c463e4c9c0f034622be4
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-08-13 12:41:12 +0300

    AUG-13

---
+++ 7-kyu/AUG-13/main.js
@@ -0,0 +1,16 @@
+/*
+Four Seven
+
+Simple kata, simple rules: your function should accept the inputs 4 and 7. If 4 is entered, the function should return 7. If 7 is entered, the function should return 4. Anything else entered as input should return 0. There's only one catch, your function cannot include if statements, switch statements, or the ternary operator (or the eval function due to the fact that you can get around the if requirement using it).
+
+There are some very simple ways of answering this problem, but I encourage you to try and be as creative as possible.
+
+Good Luck!
+*/
+
+//My solution
+function fourSeven(n){
+  let answer = {"4": 7, "7": 4};
+  let checker = {4: 4, 7: 7, undefined: 0};
+  return checker[answer[n.toString()]];
+}
