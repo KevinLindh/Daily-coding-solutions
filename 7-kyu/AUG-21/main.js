82e1ce108c073a3d69f852a2498e7589493d92b1
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-08-21 09:47:46 +0300

    AUG-21

---
+++ 7-kyu/AUG-21/main.js
@@ -0,0 +1,20 @@
+/*
+Enjoying your holiday, you head out on a scuba diving trip!
+
+Disaster!! The boat has caught fire!!
+
+You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". Then return the string.
+
+Go to work!
+*/
+
+//My solution
+function fireFight(s){
+  let newArr = s.split(" ");
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] === "Fire") {
+      newArr[i] = "~~";
+    }
+  }
+  return newArr.join(" ");
+}
