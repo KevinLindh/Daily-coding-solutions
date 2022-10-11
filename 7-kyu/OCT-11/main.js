fb81c71b16a7030e59ee49a7c016bddab1fa50b8
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-10-11 09:43:44 +0000

    OCT-11

---
+++ 7-kyu/OCT-11/main.js
@@ -0,0 +1,33 @@
+/*
+Write a function that will take a key of X and place it in the middle of Y repeated N times.
+
+Extra challege (not tested): You can complete this with under 70 characters without using regex. Challenge yourself to do this. It wont be best practices but it will work.
+
+Rules:
+
+If X cannot be placed in the middle, return X.
+
+N will always be > 0.
+
+Example:
+
+X = 'A';
+
+Y = '*';
+
+N = 10;
+
+Y repeated N times = '**********';
+
+X in the middle of Y repeated N times = '*****A*****';
+*/
+
+//My solution
+function middleMe(N, X, Y){
+  if(N%2 !== 0){
+    return X;
+  }
+  let answer = Y.repeat(N).split("");
+  answer.splice(N/2, 0, X);
+  return answer.join("");
+}
