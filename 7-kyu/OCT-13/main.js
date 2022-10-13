4b564213a9e53b356930dff3ed2ece1fe88dcee7
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-10-13 14:37:05 +0000

    OCT-13

---
+++ 7-kyu/OCT-13/main.js
@@ -0,0 +1,24 @@
+/*
+Create a function which accepts one arbitrary string as an argument, and return a string of length 26.
+
+The objective is to set each of the 26 characters of the output string to either '1' or '0' based on the fact whether the Nth letter of the alphabet is present in the input (independent of its case).
+
+So if an 'a' or an 'A' appears anywhere in the input string (any number of times), set the first character of the output string to '1', otherwise to '0'. if 'b' or 'B' appears in the string, set the second character to '1', and so on for the rest of the alphabet.
+
+For instance:
+
+"a   **&  cZ"  =>  "10100000000000000000000001"
+*/
+
+//My solution
+function change(string){
+  const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  let answer = "0".repeat(26).split("");
+  let lower = string.toLowerCase();
+  for(let i = 0; i < lower.length; i++){
+    if(alphabet.indexOf(lower[i]) !== -1){
+      answer[alphabet.indexOf(lower[i])] = "1";
+    }
+  }
+  return answer.join("");
+}
