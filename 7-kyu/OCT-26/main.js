f421f2b1d2ddea0b46e96fd47992f18814b55b62
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-10-26 08:55:50 +0000

    OCT-26

---
+++ 7-kyu/OCT-26/main.js
@@ -0,0 +1,61 @@
+/*
+Task:
+
+You have to write a function pattern which returns the following Pattern(See Pattern & Examples) upto n number of rows.
+
+    Note:Returning the pattern is not the same as Printing the pattern.
+
+Rules/Note:
+
+    If n < 1 then it should return "" i.e. empty string.
+    There are no whitespaces in the pattern.
+
+Pattern:
+
+1
+22
+333
+....
+.....
+nnnnnn
+
+Examples:
+
+    pattern(5):
+
+    1
+    22
+    333
+    4444
+    55555
+
+    pattern(11):
+
+    1
+    22
+    333
+    4444
+    55555
+    666666
+    7777777
+    88888888
+    999999999
+    10101010101010101010
+    1111111111111111111111
+
+    Hint: Use \n in string to jump to next line
+*/
+
+//My solution
+function pattern(n){
+ let output= "";
+  for(let i = 1; i <= n; i++){
+    if(i === n){
+      output += `${i}`.repeat(i);
+      break;
+    } 
+    output += `${i}`.repeat(i) + "\n";
+  }
+   
+ return output;
+}
