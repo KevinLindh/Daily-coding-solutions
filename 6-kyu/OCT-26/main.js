279df9e28ca5414ba7c7249365f5521dd56202ca
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-10-26 08:57:15 +0000

    OCT-26

---
+++ 6-kyu/OCT-26/main.js
@@ -0,0 +1,16 @@
+/*
+In this Kata, you will be given an array of arrays and your task will be to return the number of unique arrays that can be formed by picking exactly one element from each subarray.
+
+For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites. They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].
+
+Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4, since the extra outcomes are just duplicates.
+
+See test cases for more examples.
+
+Good luck!
+*/
+
+//My solution
+function solve(arr) {
+ return arr.map(val => new Set(val)).reduce((acc, val)=> acc * val.size, 1);
+};
