8dff71325ee46f6ac2e92f1bd183cac4acc916ea
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2022-11-15 11:08:54 +0000

    NOV-15

---
+++ 7-kyu/NOV-15/main.js
@@ -0,0 +1,19 @@
+/*
+In JavaScript there are truthy and falsy values:
+
+    truthy: a value that when evaluated in a boolean context returns true
+    falsy: a value that when evaluated in a boolean context returns false
+
+MDN links:
+
+    https://developer.mozilla.org/en/docs/Glossary/Truthy
+    https://developer.mozilla.org/en-US/docs/Glossary/Falsy
+
+Your task
+
+You are given two empty arrays (truthy and falsy) and you have to fill this array with at least 5 elements in each which will evaluate to true or false accordingly.
+*/
+
+//My solution
+const truthy = [1,2,3,4,5];
+const falsy = [0, null, false, NaN, undefined];
