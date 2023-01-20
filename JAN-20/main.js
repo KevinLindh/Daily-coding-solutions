3078c67465b8530f833f8cb0f94801823c273966
Kevin Lindholm <94518833+KevinLindh@users.noreply.github.com>
2023-01-20 20:26:27 +0200

    JAN-20

---
+++ JAN-20/main.js
@@ -0,0 +1,42 @@
+/*
+Description
+
+An infinite number of shelves are arranged one above the other in a staggered fashion.
+The cat can jump either one or three shelves at a time: from shelf i to shelf i+1 or i+3 (the cat cannot climb on the shelf directly above its head), according to the illustration:
+
+                 ┌────────┐
+                 │-6------│
+                 └────────┘
+┌────────┐       
+│------5-│        
+└────────┘  ┌─────► OK!
+            │    ┌────────┐
+            │    │-4------│
+            │    └────────┘
+┌────────┐  │
+│------3-│  │     
+BANG!────┘  ├─────► OK! 
+  ▲  |\_/|  │    ┌────────┐
+  │ ("^-^)  │    │-2------│
+  │ )   (   │    └────────┘
+┌─┴─┴───┴┬──┘
+│------1-│
+└────────┘
+
+Input
+
+Start and finish shelf numbers (always positive integers, finish no smaller than start)
+Task
+
+Find the minimum number of jumps to go from start to finish
+Example
+
+Start 1, finish 5, then answer is 2 (1 => 4 => 5 or 1 => 2 => 5)
+*/
+
+//My solution
+function solution(start, finish) {
+  let between = finish - start;
+  let add = between%3;
+  return Math.floor(between/3)+add;
+}
